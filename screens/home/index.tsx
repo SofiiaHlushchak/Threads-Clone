f2a8da021f8ea00e8cca8672bfd7cb8c67d5ab89
SofiiaHlushchak <glushchak02@gmail.com>
2025-07-10 21:22:38 +0200

    Creating a Linked Thread

--- screens/home/index.tsx
+++ screens/home/index.tsx
@@ -1,4 +1,4 @@
-import { Pressable, SafeAreaView } from "react-native";
+import { FlatList, Pressable, SafeAreaView } from "react-native";
 
 import ThreadsIcon from "@/assets/icons/threads";
 import {
@@ -8,12 +8,14 @@ import {
     AvatarImage,
 } from "@/components/ui/avatar";
 import { Card } from "@/components/ui/card";
+import { Divider } from "@/components/ui/divider";
 import { Heading } from "@/components/ui/heading";
 import { HStack } from "@/components/ui/hstack";
 import { Text } from "@/components/ui/text";
 import { VStack } from "@/components/ui/vstack";
+import { supabase } from "@/lib/supabase";
 import { useAuth } from "@/providers/AuthProvider";
-import { router } from "expo-router";
+import { router, usePathname } from "expo-router";
 import {
     Camera,
     Hash,
@@ -22,9 +24,27 @@ import {
     MapPin,
     Mic,
 } from "lucide-react-native";
+import { useEffect, useState } from "react";
 
 const Home = () => {
     const { user } = useAuth();
+
+    const [threads, setThreads] = useState([]);
+
+    const pathname = usePathname();
+
+    const getThreads = async () => {
+        const { data, error } = await supabase
+            .from("Post")
+            .select("*, User(*)");
+        if (!error) return setThreads(data);
+    };
+
+    useEffect(() => {
+        if (pathname === "/") {
+            getThreads();
+        }
+    }, [pathname]);
     return (
         <SafeAreaView>
             <HStack className="justify-center items-center p-3">
@@ -35,9 +55,9 @@ const Home = () => {
                     router.push("/post");
                 }}
             >
-                <HStack className="items-center p-5">
+                <HStack className="items-center px-5">
                     <Avatar size="md">
-                        <AvatarFallbackText>Jane Doe</AvatarFallbackText>
+                        <AvatarFallbackText>{user.username}</AvatarFallbackText>
                         <AvatarImage source={{ uri: user?.avatar }} />
                         <AvatarBadge />
                     </Avatar>
@@ -80,6 +100,46 @@ const Home = () => {
                         </VStack>
                     </Card>
                 </HStack>
+                <Divider />
+                <VStack space="lg">
+                    <FlatList
+                        data={threads}
+                        renderItem={({ item }) => {
+                            return (
+                                <HStack className="items-center p-3" space="sm">
+                                    <Avatar size="md">
+                                        <AvatarFallbackText>
+                                            {item.User.username}
+                                        </AvatarFallbackText>
+                                        <AvatarImage
+                                            source={{ uri: item.User?.avatar }}
+                                        />
+                                    </Avatar>
+                                    <VStack className="flex-1">
+                                        <HStack
+                                            className="items-center"
+                                            space="sm"
+                                        >
+                                            <Text size="md" bold>
+                                                {item.User.username}
+                                            </Text>
+                                            <Text
+                                                size="md"
+                                                className="text-gray-500 text-xs"
+                                            >
+                                                {new Date(
+                                                    item.created_at
+                                                ).toLocaleDateString()}
+                                            </Text>
+                                        </HStack>
+
+                                        <Text>{item.text}</Text>
+                                    </VStack>
+                                </HStack>
+                            );
+                        }}
+                    />
+                </VStack>
             </Pressable>
         </SafeAreaView>
     );
